a247436874427a97f51fd7a5e14ac6fca0589be4
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2019-08-08 17:38:54 -0400

    refined

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import MyFirstComponent from "./components/MyFirstComponent";
+import Counter from "./components/Counter";
 import "./styles.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = () => {
+  return (
+    <div className="App">
+      <MyFirstComponent />
+      <Counter count={10} />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
